ea2435397ab619485afce43b94c2d8a2dfae69cc
devbisme <devb@xess.com>
2024-02-27 08:28:24 -0500

    test: Changed suffix of generated PCB file to "kicad_pcb" so it can be opened by recent versions of PCBNEW.

--- tests/test_kinet2pcb.py
+++ tests/test_kinet2pcb.py
@@ -8,4 +8,4 @@ from kinet2pcb import *
 
 def test_1():
     """Just test to see if a netlist can be processed without an error."""
-    kinet2pcb("kinet2pcb_test/kinet2pcb_test.net", "test_1.brd")
+    kinet2pcb("kinet2pcb_test/kinet2pcb_test.net", "test_1.kicad_pcb")
